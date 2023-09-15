5b1b40273dd82b9f0a76376b4b02bf7c9596f167
Luis Mendes <machadoluismendes@gmail.com>
2023-09-15 13:53:17 -0300

    infra.ses layer added

--- src/main/java/com/github/luismendes070/emailservice/controllers/EmailSenderController.java
+++ src/main/java/com/github/luismendes070/emailservice/controllers/EmailSenderController.java
@@ -1,7 +1,13 @@
 package com.github.luismendes070.emailservice.controllers;
 
+import com.github.luismendes070.emailservice.core.EmailRequest;
 import com.github.luismendes070.emailservice.application.EmailSenderService;
+import com.github.luismendes070.emailservice.core.exceptions.EmailServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +20,18 @@ public class EmailSenderController {
     @Autowired
     public EmailSenderController(EmailSenderService emailSenderService){
         this.emailSenderService = emailSenderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
+
+        try{
+            this.emailSenderService.sendEmail(request.to() , request.subject(), request.body());
+            return  ResponseEntity.ok("email send sucessfully.");
+        }catch (EmailServiceException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while sending email");
+        }
+
     }
 
 }
---
+++ src/main/java/com/github/luismendes070/emailservice/core/EmailRequest.java
@@ -0,0 +1,7 @@
+package com.github.luismendes070.emailservice.core;
+
+public record EmailRequest(String to, String subject, String body) {
+
+
+
+}
